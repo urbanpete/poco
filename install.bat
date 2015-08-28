754ffdaed38d38eded3c2d8a31ae7e9f95637bd0
Peter Gissel <pgissel@airware.com>
2015-08-28 08:33:17 -0700

    call vsvars from batch file

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-call buildwin 140 clean
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 call buildwin 140 build shared both x64 nosamples notests
 mkdir poco-x86_64-win-msvc
 mkdir poco-x86_64-win-msvc\include
