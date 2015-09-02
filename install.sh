06a997a143cb6079853de84c5797e458d9783d4e
Peter Gissel <pgissel@airware.com>
2015-09-01 17:09:58 -0700

    fix linux install script to copy files to correct location

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mkdir poco-x86_64-linux-gcc/lib
 mkdir poco-x86_64-linux-gcc/include
 mkdir poco-x86_64-linux-gcc/include/Poco
 cp -PR lib/Linux/x86_64/*.* poco-x86_64-linux-gcc/lib
-cp -rf ./Foundation/include/Poco poco-x86_64-linux-gcc/include/Poco 
+cp -rf ./Foundation/include/Poco/*.* poco-x86_64-linux-gcc/include/Poco 
 cp -rf ./Util/include/Poco/Util poco-x86_64-linux-gcc/include/Poco/Util 
 cp -rf ./XML/include/Poco/XML poco-x86_64-linux-gcc/include/Poco/XML 
 cp -rf ./XML/include/Poco/DOM poco-x86_64-linux-gcc/include/Poco/DOM 
