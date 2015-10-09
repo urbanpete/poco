3f8bff60e1204cd854187941c1abb2549e09fd45
Peter Gissel <pgissel@airware.com>
2015-10-09 11:08:06 -0700

    change build script to 32 bit

--- install64.bat
+++ install64.bat
@@ -1,20 +1,20 @@
-rm -rf bin
-rm -rf lib
+rm -rf bin64
+rm -rf lib64
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 cd CppUnit
-"%AIRWARE_MSBUILD15%\msbuild" CppUnit_vs140.sln /property:Configuration=%1 /m
+"%AIRWARE_MSBUILD15%\msbuild" CppUnit_x64_vs140.sln /property:Configuration=%1 /m
 cd ..
 cd Foundation
-"%AIRWARE_MSBUILD15%\msbuild" Foundation_vs140.sln /property:Configuration=%1 /m
+"%AIRWARE_MSBUILD15%\msbuild" Foundation_x64_vs140.sln /property:Configuration=%1 /m
 cd ..
 cd XML
-"%AIRWARE_MSBUILD15%\msbuild" XML_vs140.sln /property:Configuration=%1 /m
+"%AIRWARE_MSBUILD15%\msbuild" XML_x64_vs140.sln /property:Configuration=%1 /m
 cd ..
 cd JSON
-"%AIRWARE_MSBUILD15%\msbuild" JSON_vs140.sln /property:Configuration=%1 /m
+"%AIRWARE_MSBUILD15%\msbuild" JSON_x64_vs140.sln /property:Configuration=%1 /m
 cd ..
 cd Util
-"%AIRWARE_MSBUILD15%\msbuild" Util_vs140.sln /property:Configuration=%1 /m
+"%AIRWARE_MSBUILD15%\msbuild" Util_x64_vs140.sln /property:Configuration=%1 /m
 cd ..
 mkdir poco-x86_64-win-msvc-%1
 mkdir poco-x86_64-win-msvc-%1\include
@@ -25,7 +25,7 @@ xcopy Foundation\include\Poco\*.* poco-x86_64-win-msvc-%1\include\Poco /s /Y
 xcopy Util\include\Poco\*.* poco-x86_64-win-msvc-%1\include\Poco /s /Y
 xcopy XML\include\Poco\*.* poco-x86_64-win-msvc-%1\include\Poco /s /Y
 xcopy JSON\include\Poco\*.* poco-x86_64-win-msvc-%1\include\Poco /s /Y
-copy bin\*.dll poco-x86_64-win-msvc-%1\bin /Y
-copy lib\*.lib poco-x86_64-win-msvc-%1\lib /Y
+copy bin64\*.dll poco-x86_64-win-msvc-%1\bin /Y
+copy lib64\*.lib poco-x86_64-win-msvc-%1\lib /Y
 copy readme*.md poco-x86_64-win-msvc-%1
 copy pocoConfig.cmake poco-x86_64-win-msvc-%1
